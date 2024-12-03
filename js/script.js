ad28eb11fdfd563e9e7931a9119115c7b325af81
John <186672498+johntypescode@users.noreply.github.com>
2024-12-03 03:36:18 -0500

    1. paragraph with red text

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ window.onload = () => {
 	// Load the parent container into memory
 	const parentDIV = document.querySelector('div#parent-container');
 
-	// Just making sure everything is set up correctly...
-	alert(parentDIV);
+	/**
+	 * Exercise 1: a <p> with red text that says "Hey, I'm red!"
+	 */
+
+	const redParagraph = document.createElement('p');
+	redParagraph.style.color = 'red';
+	redParagraph.textContent = 'Hey, I\'m red!';
+
+	// Append the red text paragraph to parent container
+	parentDIV.appendChild(redParagraph); // SUCCESS
 
 };
