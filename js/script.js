bb3c7cc6374cb1e60768f5dc30e35c12557dcd0b
John <186672498+johntypescode@users.noreply.github.com>
2024-12-03 03:55:54 -0500

    3. Pink DIV with black border; h1 and paragraph within, appended to parent

--- js/script.js
+++ js/script.js
@@ -23,4 +23,31 @@ window.onload = () => {
 
 	parentDIV.appendChild(blueH3);
 
+	/**
+	 * Exercise 3: <div> with black border and pink background
+	 * 				-> <h1> "I'm in a DIV!"
+	 * 				-> <p> "Me too!"
+	 */
+
+	// The pink DIV container
+	const pinkDIV = document.createElement('div');
+	pinkDIV.style.border = '3px solid #000';
+	pinkDIV.style.backgroundColor = 'pink';
+
+	// H1 in pink DIV
+	const H1inDIV = document.createElement('h1');
+	H1inDIV.textContent = 'I\'m in a DIV!';
+
+	// Paragraph in pink DIV
+	const PgInDIV = document.createElement('p');
+	PgInDIV.textContent = 'ME TOO!';
+
+	// Append the H1 to the pink DIV
+	pinkDIV.appendChild(H1inDIV);
+	pinkDIV.appendChild(PgInDIV);
+
+	// Append the pink DIV to the container parent
+	parentDIV.appendChild(pinkDIV);
+
+
 };
