2ac5cfc88a3dd73f5d32007d383673180dd65392
John <186672498+johntypescode@users.noreply.github.com>
2024-12-03 03:29:44 -0500

    querySelector() test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 window.onload = () => {
 
+	// Load the parent container into memory
+	const parentDIV = document.querySelectory('div#parent-container');
+
+	// Just making sure everything is set up correctly...
+	alert(parentDIV);
+
 };
